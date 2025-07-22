90becbb826eefd0e2465a2ddd2647182526f358f
Wiciu021 <mikolajwitkowski27@gmail.com>
2025-07-22 19:28:15 +0200

    Fresh start

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const { PrismaClient } = require('@prisma/client')
+
+const app = express()
+const prisma = new PrismaClient() //prismą zajme sie optem ale to jest potrzebne jak coś
+
+app.use(cors()) // żądania z frontend
+app.use(helmet()) // gowno ale w poradniku mówią że to bezpieczne
+app.use(morgan('dev')) // loguje żądania
+app.use(express.json())
